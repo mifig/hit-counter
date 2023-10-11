b60a2aeac8c71ac37910787d8293ca4ec1507e0d
Miguel Figueiredo <miguelpfigueiredo@gmail.com>
2023-10-11 17:03:18 +0100

    finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,16 @@
 import React from 'react';
-
-import {
-  readFile,
-  writeFile,
-} from '../helpers/file-helpers';
-
-import Button from '../components/Button';
-
-const DATABASE_PATH = '/src/database.json';
-
-/*
-`readFile` takes 1 argument:
-• the path to the file:
-
-readFile('/path/to/file');
-
-`writeFile` takes 2 arguments:
-• The path to the file
-• The new contents for the file
-
-writeFile(
-  '/path/to/file',
-  '{ "hello": "world" }'
-);
-*/
+import HitCounter from '../components/HitCounter';
+import Censored from '../components/Censored';
 
 function Home() {
-  const count = JSON.parse(readFile(DATABASE_PATH))
-  count.hits++
-  writeFile(DATABASE_PATH, JSON.stringify(count))
-
   return (
     <main>
       <h1>Welcome!</h1>
-      <p>You are visitor number <Button hits={count.hits}/></p>
+      <p>You are visitor number {" "}
+        <Censored>
+          <HitCounter />
+        </Censored>
+      </p>
     </main>
   );
 }
